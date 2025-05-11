0ef0c796169cd14c3710a4b1ce8b142e33237a40
BuildTools <unconfigured@null.spigotmc.org>
2025-05-11 10:39:32 +0300

    Initial commit

---
+++ src/main/java/dev/mcserver/mcservermod/MCSERVER.java
@@ -0,0 +1,17 @@
+package dev.mcserver.mcservermod;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class MCSERVER implements ModInitializer {
+	public static final String MOD_ID = "mcserver";
+
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/dev/mcserver/mcservermod/MCSERVERClient.java
@@ -0,0 +1,11 @@
+package dev.mcserver.mcservermod;
+
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.mixin.networking.client.accessor.ClientCommonNetworkHandlerAccessor;
+
+public class MCSERVERClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
